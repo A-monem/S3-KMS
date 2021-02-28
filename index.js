d18d48e2cd043ff41fe1108a2f29271de123b6c0
Abdelmoneim Nafea <aamnafea@hotmail.com>
2021-02-28 13:52:52 +1100

    Encryption

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 const Promise = require('bluebird');
 const AWS = require('aws-sdk');
 const fs = require('fs');
+const os = require('os');
 
 Promise.promisifyAll(fs);
 
@@ -11,17 +13,20 @@ const SECRET_ACCESS_KEY = 'Hl084rcoXwdC5E1MSSxvGUPF3fjGkDmDaRNVKa2w';
 // bucket name
 const bucketName = 'conformity-anafea';
 const localDirectory = 'data';
+const downloadedFileName = 'data/downloadedFiles.txt';
 
 const options = {
   accessKeyId: ACCESS_KEY_ID,
   secretAccessKey: SECRET_ACCESS_KEY,
+  region: 'ap-southeast-2',
 };
 
 const s3 = new AWS.S3(options);
+const kms = new AWS.KMS(options);
 
 function clearDataDirectory() {
-  fs.rmdirSync('data', { recursive: true });
-  fs.mkdirSync('data');
+  fs.rmdirSync(localDirectory, { recursive: true });
+  fs.mkdirSync(localDirectory);
 }
 
 function createDirectory(directories) {
@@ -39,7 +44,7 @@ function sortBucketObjects(data) {
 
 function downloadFile(file, i) {
   return new Promise((resolve) => {
-    console.log(`File ${i + 1} downloading ...`);
+    console.log(`File ${i + 1} downloading started`);
 
     const params = {
       Bucket: bucketName,
@@ -50,12 +55,57 @@ function downloadFile(file, i) {
       if (err) {
         console.log(err, err.stack);
       } else {
-        fs.writeFileAsync(`${localDirectory}/${file.Key}`, data.Body).then(() => {
-          console.log(`File ${i + 1} done`);
-          resolve();
-        });
+        fs.writeFileAsync(`${localDirectory}/${file.Key}`, data.Body)
+          .then(() => {
+            console.log(`File ${i + 1} downloading finished`);
+
+            const appendFileNamePromise = new Promise((resolveAppend) => (
+              fs.appendFileAsync(downloadedFileName, `${file.Key.match(/\w*.\w{3}$/i)[0]}${os.EOL}`, 'utf8')
+                .then(() => resolveAppend())
+            ));
+
+            resolve(appendFileNamePromise);
+          })
+          .catch((error) => console.log(error, error.stack));
       }
     });
+  });
+}
+
+//
+
+// function decryptFile(file) {
+//   const params = {
+//     KeyId: 'alias/conformity',
+//     CiphertextBlob: Buffer.from(file, 'base64'),
+//   };
+
+//   kms.decrypt(params, (err, data) => {
+//     if (err) {
+//       throw err;
+//     } else {
+//       console.log(data.Plaintext.toString('utf-8'));
+//     }
+//   });
+// }
+
+function encryptFile(file) {
+  const params = {
+    KeyId: 'alias/conformity',
+    Plaintext: Buffer.from(fs.readFileSync(file)),
+  };
+
+  kms.encrypt(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      const { CiphertextBlob } = data;
+
+      fs.truncateSync(downloadedFileName, 0);
+      fs.writeFile(downloadedFileName, CiphertextBlob.toString('base64'), () => {
+        console.log('downloadedFiles.txt has been successfully encrypted');
+      });
+    }
   });
 }
 
@@ -68,6 +118,15 @@ s3.listObjects({ Bucket: bucketName }, (err, data) => {
     clearDataDirectory();
     createDirectory(directories);
 
-    Promise.map(files, (file, i) => downloadFile(file, i), { concurrency: 4 });
+    Promise.map(files, (file, i) => downloadFile(file, i), { concurrency: 4 })
+      .then((fileNames) => {
+        Promise.all(fileNames);
+      })
+      .then(() => {
+        console.log('*************************************************');
+        console.log('downloadedFiles.txt has been successfully created');
+        encryptFile(downloadedFileName);
+      })
+      .catch((error) => console.log(error, error.stack));
   }
 });
